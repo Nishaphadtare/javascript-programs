097e812e73b926fbd36f30ae6f643a921c5c2ac5
Nishaphadtare <nishaphadtare03@gmail.com>
2024-11-11 20:24:04 +0530

    JavaScript variables
    JavaScript variables

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+
+// Variable Declaration
+var firstName;
+console.log(firstName); //o/p: Undefined
+
+// Variable initialization
+
+firstName = "Jenny";
+console.log(firstName);
+
+// Variable Declaration and initialization
+var lastName = "Gates";
+console.log(lastName);
+
+// Variable Update
+firstName = "Jennifer"
+console.log(firstName);
+
+
---
+++ 01_variableAssign.js
@@ -0,0 +1,41 @@
+
+console.log("Log your city name on console");
+
+var city = "Solapur"
+console.log(city);
+
+console.log("-----------------------------------------------------------------");
+
+console.log("Log your best friend name on console");
+var bestFriend = "Sneha"
+console.log(bestFriend);
+
+console.log("-----------------------------------------------------------------");
+console.log("Log your country name on console");
+var country = "India";
+console.log(country);
+
+console.log("-----------------------------------------------------------------");
+console.log("Log your school name on console");
+var school = "B.V.Ankoli";
+console.log(school);
+
+console.log("-----------------------------------------------------------------");
+console.log("Log your college name on console");
+var college = "DSG college Mohol"
+console.log(college);
+
+// console.log("-----------------------------------------------------------------");
+
+// console.log("My city Name is = 'Mumbai'");
+// console.log("My country Name is = 'India'");
+// console.log("My best friend Name is = 'Sneha'");
+// console.log("My school Name is = 'B.V.Ankoli'");
+// console.log("My college Name is = 'DSG college'");
+
+
+
+
+
+
+
